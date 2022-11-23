4b4dc3c1bc0a8bd6b048e37962a6d045c0d3e7c0
sshknt5 <63108010+sshknt5@users.noreply.github.com>
2022-11-23 14:53:15 +0530

    Add files via upload

--- 23.d.py
+++ 23.d.py
@@ -1,4 +1,28 @@
-vehicle = input("what type of vechicle")
+from datetime import datetime
 
+vehicle = input("what type of vechicle")
+str1 = (input("enter entry time"))
+str2 = (input("enter exit time"))
+start_time = datetime.strptime(str1, "%H:%M:%S")
+end_time = datetime.strptime(str2, "%H:%M:%S")
+delta = end_time - start_time
+sec = delta.total_seconds()
+min = sec / 60
+hrs = sec / (60 * 60)
+total = 0
 if(vehicle == "car"):
-    print(1)
+    if(hrs<=3):
+        total = hrs*10
+    else:
+        total = 30+20*(hrs-3)
+elif(vehicle == "truck" or vehicle == "bus"):
+    if(hrs<=3):
+        total = hrs*20
+    else:
+        total = (hrs-3)*30+60
+elif(vehicle == "cycle" or vehicle == "motorcycle" or vehicle == "scooter"):
+    if(hrs<=3):
+        total = hrs*5
+    else:
+        total = 15+(hrs-3)*10
+print(total)
