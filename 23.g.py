28dc4982f325860908392c92c3e9ebc9ae2e8953
sshknt5 <63108010+sshknt5@users.noreply.github.com>
2022-11-30 14:06:32 +0530

    Update 23.g.py

--- 23.g.py
+++ 23.g.py
@@ -1,19 +1,11 @@
-import math
+str = input("enter any number")
+strlen = len(str)
+j=1
+for i in range(0,len(str)):
+    print(" "*i,end="")
+    print(str[i:],end="")
+    print(" "*5,end="")
+    print(str[:j])
+    j+=1
+    
 
-def makepattern(a):
-    while(a>0):
-        count=10
-        count2=2
-        tempa = a
-        temp = tempa%10000
-        print("{}            {}".format(a,temp))
-        a = a//10
-        count=math.pow(count,count2)
-        count2 = count2+1
-
-a =int(input("enter any 5 digit number"))
-b = (len(str(a)))
-if(b!=5):
-    print("invalid input")
-else:
-    makepattern(a)
